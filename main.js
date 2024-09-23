091aac6610a2d70e8692159ff3bcbe3939d9f101
Matias Suarez <166583624+MatiSuarez2311@users.noreply.github.com>
2024-09-23 16:52:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,46 +1,27 @@
 
-const Productos = [
-    {
-        titulo: "Bufanda HARRY POTTER - SLYTHERIN",
-        imagen: "https://acdn.mitiendanube.com/stores/003/702/378/products/bufanda-harry-potter-slytherin-frase1-465e895b672b0e77de16230257723865-1024-1024.jpg",
-        precio: 10000,
-    },
-    {
-        titulo: "Bufanda HARRY POTTER - GRYFFINDOR",
-        imagen: "https://http2.mlstatic.com/D_NQ_NP_804812-MLU75501871277_032024-O.webp",
-        precio: 10000,
-    },
-    {
-        titulo: "Bufanda HARRY POTTER - REEVENCLAW",
-        imagen: "https://acdn.mitiendanube.com/stores/907/020/products/1024x1024_bufanda-ravenclaw-escrita-1-harry-potter-abracadabra-juguetes1-db7174168fe996f46a15908565250660-640-0.jpg",
-        precio: 10000,
-    },
-    {
-        titulo: "Bufanda HARRY POTTER - HUFFLEPUFF",
-        imagen: "https://http2.mlstatic.com/D_NQ_NP_745764-MLA43462845079_092020-O.webp",
-        precio: 10000,
-    },
-    {
-        titulo: "Funko Pop POKEMON - MEWTWO",
-        imagen: "https://http2.mlstatic.com/D_NQ_NP_641293-MLM43571955301_092020-O.webp",
-        precio: 9000,
-    },
-    {
-        titulo: "Funko Pop DRAGONBALLZ - VEGETA",
-        imagen: "https://dcdn.mitiendanube.com/stores/113/368/products/vegetared1-6527d00915478ef60416096323644724-640-0.png",
-        precio: 15000,
-    },
-    {
-        titulo: "Funko Pop STAN LEE",
-        imagen: "https://http2.mlstatic.com/D_NQ_NP_836818-MLA43484752357_092020-O.webp",
-        precio: 15000,
-    },
-    {
-        titulo: "Funko Pop RICK & MORTY - KING OF $#!+",
-        imagen: "https://images.stockx.com/images/Funko-Pop-Animation-Rick-Morty-King-of-with-Sound-Figure-694.jpg?fit=fill&bg=FFFFFF&w=700&h=500&fm=webp&auto=compress&q=90&dpr=2&trim=color&updated_at=1649962855",
-        precio: 20000,
-    },
-]
+
+
+const creadoraDeCard = (titulo, precio, imagen) => {
+    main.innerHTML += `
+        <div>
+            <h3>${titulo}</h3>
+            <p>${precio}</p>
+            <img src="${imagen}" alt="">
+            <button>comprar</button>
+        </div>`;
+};
+const llamadoraDeProductos = async()=>{
+    let resp = await fetch("./info.json")
+    let data = await resp.json()
+    data.forEach(el =>{
+        creadoraDeCard(el.titulo, el.precio, el.imagen)
+    Productos = data; 
+    mostrarProductos(Productos);
+    })
+    
+}
+
+llamadoraDeProductos()
 
 const Carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
@@ -50,7 +31,7 @@ const carritoItems = document.getElementById("carritoItems");
 const finalizarCompra = document.getElementById("finalizarCompra");
 
 
-const creadoraDeCards = (titulo, imagen, precio) => {
+const creadoraDeCards = (titulo, precio, imagen) => {
     const card = document.createElement("div");
     card.className = "producto";
 
@@ -193,18 +174,23 @@ finalizarCompra.addEventListener("click", () => {
             }
         });
 
-        // Envío de los datos del carrito a una API
-        fetch('https://api.example.com/checkout', { // URL de la API
+       
+        fetch('/url-del-servidor', {  // Reemplaza con tu URL
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                items: Carrito,    // Detalles del carrito
-                total: total       // Total de la compra
+                items: Carrito,
+                total: total
             })
         })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
         .then(data => {
             Swal.fire({
                 title: '¡Gracias por su compra!',
